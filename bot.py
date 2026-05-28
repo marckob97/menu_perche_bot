491964219d6ddb7029947c5969a308c9be18d6d0
marckob97 <marckob.97@gmail.com>
2026-05-28 09:08:43 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,15 @@ def prendi_menu():
 
     url = "https://www.ristoranteperche.com/menu-del-giorno-pranzo"
 
-    response = requests.get(url)
+    headers = {
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/122.0.0.0 Safari/537.36"
+    )
+}
+
+response = requests.get(url, headers=headers)
 
     soup = BeautifulSoup(response.text, "html.parser")
 
